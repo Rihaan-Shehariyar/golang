aca6c6ca99202490783bd884a4b74e7209fe78f7
Rihaan Shehariyar <rihaanshehariyar@gmail.com>
2026-02-22 23:23:42 +0530

    updated

--- Go-rev/gorm/main.go
+++ Go-rev/gorm/main.go
@@ -12,40 +12,39 @@ type User struct {
 	ID         uint   `gorm:"primaryKey"`
 	Name       string `gorm:"not null"`
 	Email      string `gorm:"uniqueIndex"`
-    Orders []Order
+	Orders     []Order
 	Created_at time.Time
 }
 
-type Order struct{
-  ID uint `gorm:"primaryKey"`
-  UserId uint `gorm:"foriegnKey"`
-  Total int
-  Created_at time.Time 
+type Order struct {
+	ID         uint `gorm:"primaryKey"`
+	UserId     uint `gorm:"foriegnKey"`
+	Total      int
+	Created_at time.Time
 }
 
 var DB *gorm.DB
 
-func Connect(){
- dsn := "host=localhost user=postgres password=Rihaan@123 dbname=ecommerce port=5432 sslmode=disable"
+func Connect() {
+	dsn := "host=localhost user=postgres password=Rihaan@123 dbname=ecommerce port=5432 sslmode=disable"
 
- db,err := gorm.Open(postgres.Open(dsn),&gorm.Config{})
- 
-if err!=nil {
-	log.Fatalf("error while connecting")
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalf("error while connecting")
+	}
+
+	DB = db
+
 }
- 
-DB=db
 
+func main() {
+	Connect()
 
-}
+	DB.AutoMigrate(&User{})
 
-func main(){
-Connect()
-
- DB.AutoMigrate(&User{})
-
- DB.Create(&User{
- Name: "name",
-})
+	DB.Create(&User{
+		Name: "name",
+	})
 
 }
