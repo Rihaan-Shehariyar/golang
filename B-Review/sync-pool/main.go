31444f76467dbef589a2308c9a34b74b88a0b671
Rihaan Shehariyar <rihaanshehariyar@gmail.com>
2026-02-25 11:08:54 +0530

    updated

---
+++ B-Review/sync-pool/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var pool = sync.Pool{
+ New: func() any {
+   fmt.Println("creating a new Buffer")
+   return make([]byte,0,10)
+},
+}
+
+
+func worker(id int,wg *sync.WaitGroup){
+
+ defer wg.Done()
+
+ buff := pool.Get().([]byte)
+
+ buff = append(buff, byte(id))
+
+ fmt.Println("worker",id,"buff:",buff)
+
+ buff = buff[:0]
+ pool.Put(buff)
+}
+
+func main(){
+ 
+ var wg sync.WaitGroup
+
+ for i := 0; i < 5; i++ {
+	wg.Add(1)
+    go worker(i,&wg)
+ }
+
+ wg.Wait()
+ 
+}
