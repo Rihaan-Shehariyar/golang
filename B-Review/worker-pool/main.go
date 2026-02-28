551dc5652b4a0811ad1cd8a82d8577eae4d549d5
Rihaan Shehariyar <rihaanshehariyar@gmail.com>
2026-02-28 10:47:35 +0530

    updated

--- B-Review/worker-pool/main.go
+++ B-Review/worker-pool/main.go
@@ -1,63 +1,100 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"sync"
 	"time"
 )
 
-func Worker(ctx context.Context, id int, jobs <-chan int, wg *sync.WaitGroup) {
+// import (
+// 	"context"
+// 	"fmt"
+// 	"sync"
+// 	"time"
+// )
 
+// func Worker(ctx context.Context, id int, jobs <-chan int, wg *sync.WaitGroup) {
+
+// 	defer wg.Done()
+
+// 	for {
+// 		select {
+// 		case job, ok := <-jobs:
+
+// 			if !ok {
+// 				fmt.Println("Worker", id, "Stopping")
+// 				return
+// 			}
+
+// 			fmt.Println("worker", id, "Proccesing", job)
+// 			time.Sleep(time.Second)
+
+// 		case <-ctx.Done():
+// 			fmt.Println("Worker", id, "Cancelled")
+// 			return
+// 		}
+// 	}
+
+// }
+
+// func main() {
+
+// 	ctx, cancel := context.WithCancel(context.Background())
+// 	defer cancel()
+
+// 	jobs := make(chan int)
+// 	var wg sync.WaitGroup
+
+// 	workCount := 3
+
+// 	for i := 0; i < workCount; i++ {
+
+// 		wg.Add(1)
+// 		go Worker(ctx, i, jobs, &wg)
+
+// 	}
+
+// 	go func() {
+// 		for j := 0; j <= 7; j++ {
+// 			jobs <- j
+// 		}
+
+// 		close(jobs)
+// 	}()
+
+// 	wg.Wait()
+
+// 	fmt.Println("All workers Finished")
+
+// }
+
+func worker(id int, jobs <-chan int, wg *sync.WaitGroup) {
 	defer wg.Done()
 
-	for {
-		select {
-		case job, ok := <-jobs:
-
-			if !ok {
-				fmt.Println("Worker", id, "Stopping")
-				return
-			}
-
-			fmt.Println("worker", id, "Proccesing", job)
-			time.Sleep(time.Second)
-
-		case <-ctx.Done():
-			fmt.Println("Worker", id, "Cancelled")
-			return
-		}
+	for job := range jobs {
+		fmt.Println("worker", id, "processing", job)
+		time.Sleep(time.Second)
 	}
 
 }
 
 func main() {
+	jobs := make(chan int, 10)
 
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	jobs := make(chan int)
 	var wg sync.WaitGroup
 
-	workCount := 3
-
-	for i := 0; i < workCount; i++ {
-
+	for i := 1; i <= 3; i++ {
 		wg.Add(1)
-		go Worker(ctx, i, jobs, &wg)
+		go worker(i, jobs, &wg)
+	}
+
+	for j := 1; j <= 10; j++ {
+
+		jobs <- j
 
 	}
 
-	go func() {
-		for j := 0; j <= 7; j++ {
-			jobs <- j
-		}
-
-		close(jobs)
-	}()
-
-	wg.Wait()
-
-	fmt.Println("All workers Finished")
+	close(jobs)
+   wg.Wait()
 
 }
