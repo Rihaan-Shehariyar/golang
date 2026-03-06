825297bb22620fec648d3bd3968507b006f7d0cd
Rihaan Shehariyar <rihaanshehariyar@gmail.com>
2026-03-06 10:46:40 +0530

    updated

--- sorting/main.go
+++ sorting/main.go
@@ -25,7 +25,24 @@ func BubbleSort(arr []int) {
 
 }
 
-func main(){
+func InsertionSort(arr []int) {
+
+	for i := 1; i < len(arr); i++ {
+
+    key := arr[i]
+     j := i-1
+  for j >= 0 && arr[j] > key{
+    arr[j+1] = arr[j]
+    j--
+}
+
+  arr[j+1] = key
+ 
+	}
+
+}
+
+func main() {
 
 	arr := []int{5, 6, 1, 7, 8, 8}
 	BubbleSort(arr)
