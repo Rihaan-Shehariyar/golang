b9749c4da315ec29a387c66e5faaff71490f6492
Rihaan Shehariyar <rihaanshehariyar@gmail.com>
2026-03-06 10:47:35 +0530

    updated

--- sorting/main.go
+++ sorting/main.go
@@ -29,15 +29,15 @@ func InsertionSort(arr []int) {
 
 	for i := 1; i < len(arr); i++ {
 
-    key := arr[i]
-     j := i-1
-  for j >= 0 && arr[j] > key{
-    arr[j+1] = arr[j]
-    j--
-}
+		key := arr[i]
+		j := i - 1
+		for j >= 0 && arr[j] > key {
+			arr[j+1] = arr[j]
+			j--
+		}
 
-  arr[j+1] = key
- 
+		arr[j+1] = key
+
 	}
 
 }
