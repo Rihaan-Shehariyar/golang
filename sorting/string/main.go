5f30fa78874e9779ac3cfb209b8d842eca5b3826
Rihaan Shehariyar <rihaanshehariyar@gmail.com>
2026-03-07 10:45:33 +0530

    updated

---
+++ sorting/string/main.go
@@ -0,0 +1,19 @@
+package main
+
+func ReverseString(s string) string {
+	b := []byte(s)
+
+	i := 0
+	j := len(b) - 1
+
+	for i < j {
+
+		b[i], b[j] = b[j], b[i]
+		i++
+		j--
+
+	}
+
+	return string(b)
+
+}
