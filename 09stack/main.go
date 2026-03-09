6e2fd42bfeb79617f02509c8a1c9fdedee273257
Rihaan Shehariyar <rihaanshehariyar@gmail.com>
2026-03-09 20:22:52 +0530

    updated

--- 09stack/main.go
+++ 09stack/main.go
@@ -1,59 +1,58 @@
-package main
+// package stack
 
-import "fmt"
+// import "fmt"
 
-type Stack struct {
-	items []int
-}
+// type Stack struct {
+// 	items []int
+// }
 
-func (s *Stack) Push(value int) {
+// func (s *Stack) Push(value int) {
 
-	s.items = append(s.items, value)
-}
+// 	s.items = append(s.items, value)
+// }
 
-func (s *Stack) Pop() int {
+// func (s *Stack) Pop() int {
 
-	if len(s.items) == 0 {
+// 	if len(s.items) == 0 {
+// 		return -1
+// 	}
 
-		return -1
-	}
+// 	top := s.items[len(s.items)-1]
+// 	s.items = s.items[:len(s.items)-1]
+// 	return top
+// }
 
-	top := s.items[len(s.items)-1]
-	s.items = s.items[:len(s.items)-1]
-	return top
-}
+// func (s *Stack) peek()int{
 
-func (s *Stack) peek()int{
-
- if len(s.items) == 0{
+//  if len(s.items) == 0{
   
-   return -1
-}
+//    return -1
+// }
   
-  return s.items[len(s.items)-1]
-}
+//   return s.items[len(s.items)-1]
+// }
 
-func main() {
+// func main() {
 
-	s := Stack{}
+// 	s := Stack{}
 
-	s.Push(10)
-	s.Push(20)
-	s.Push(30)
-	s.Push(40)
-	s.Push(50)
-	s.Push(60)
-
-
-	fmt.Println(s)
-
-s.Pop()
-s.Pop()
-s.Pop()
-s.Pop()
+// 	s.Push(10)
+// 	s.Push(20)
+// 	s.Push(30)
+// 	s.Push(40)
+// 	s.Push(50)
+// 	s.Push(60)
 
 
-fmt.Println(s)
-fmt.Println(s.peek())
+// 	fmt.Println(s)
 
-}
+// s.Pop()
+// s.Pop()
+// s.Pop()
+// s.Pop()
+
+
+// fmt.Println(s)
+// fmt.Println(s.peek())
+
+// }
