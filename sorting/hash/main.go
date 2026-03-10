28d5c9a5dcb8ee2b7e529a12792d324d8dc90be5
Rihaan Shehariyar <rihaanshehariyar@gmail.com>
2026-03-10 17:26:51 +0530

    updated

---
+++ sorting/hash/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+
+	text := "Hello Good Morning Hello Good Night"
+	freq := make(map[string]int)
+
+	words := strings.Fields(text)
+
+	for _, word := range words {
+		freq[word]++
+	}
+ 
+ fmt.Println(freq)
+
+}
