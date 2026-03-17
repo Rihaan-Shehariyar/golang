226acc60eee40f2585456545e87feaba1f6ddd9d
Rihaan Shehariyar <rihaanshehariyar@gmail.com>
2026-03-17 16:50:43 +0530

    updated

--- sorting/heap/main.go
+++ sorting/heap/main.go
@@ -1,113 +1,95 @@
 package main
 
-// import "fmt"
-
-// type Heap struct {
-// 	data []int
-// }
-
-// func (h *Heap) MinInsert(value int) {
-
-// 	h.data = append(h.data, value)
-// 	index := len(h.data) - 1
-
-// 	for index > 0 {
-// 		parent := (index - 1) / 2
-
-// 		if h.data[index] < h.data[parent] {
-// 			h.data[index], h.data[parent] = h.data[parent], h.data[index]
-// 			index = parent
-// 		} else {
-// 			break
-// 		}
-// 	}
-
-// }
-
-// func (h *Heap) MaxInsert(value int) {
-
-// 	h.data = append(h.data, value)
-// 	index := len(h.data) - 1
-
-// 	for index > 0 {
-// 		parent := (index - 1) / 2
-
-// 		if h.data[index] > h.data[parent] {
-// 			h.data[index], h.data[parent] = h.data[parent], h.data[index]
-// 			index = parent
-// 		} else {
-// 			break
-
-// 		}
-
-// 	}
-
-// }
-
-// func (h *Heap) ExtractMIn() int {
-
-// 	if len(h.data) == 0 {
-// 		return -1
-// 	}
-
-// 	min := h.data[0]
-
-// 	last := h.data[len(h.data)-1]
-// 	h.data[0] = last
-// 	h.data = h.data[:len(h.data)-1]
-
-// 	return min
-// }
-
-// func (h *Heap) HeapifyDown(index int) {
-
-// 	size := len(h.data)
-
-// 	for {
-
-// 		left := 2*index + 1
-// 		right := 2*index + 2
-// 		smallest := index
-
-// 		if left < size && h.data[left] < h.data[smallest] {
-// 			smallest = left
-// 		}
-
-// 		if right < size && h.data[right] < h.data[smallest] {
-// 			smallest = right
-// 		}
-
-// 		if index == smallest {
-// 			break
-// 		}
-
-// 		h.data[index], h.data[smallest] = h.data[smallest], h.data[index]
-// 		index = smallest
-
-// 	}
-
-// }
-
-// func main() {
-
-// 	h := Heap{}
-// 	x := Heap{}
-// 	h.MinInsert(10)
-// 	h.MinInsert(5)
-// 	h.MinInsert(30)
-// 	h.MinInsert(2)
-// 	h.MinInsert(1)
-// 	x.MaxInsert(10)
-// 	x.MaxInsert(20)
-// 	x.MaxInsert(50)
-
-// 	fmt.Println(h.data)
-// 	fmt.Println(x.data)
-// }
+import "fmt"
 
 type Heap struct {
 	data []int
 }
+
+
+
+func (h *Heap) MaxInsert(value int) {
+
+	h.data = append(h.data, value)
+	index := len(h.data) - 1
+
+	for index > 0 {
+		parent := (index - 1) / 2
+
+		if h.data[index] > h.data[parent] {
+			h.data[index], h.data[parent] = h.data[parent], h.data[index]
+			index = parent
+		} else {
+			break
+
+		}
+
+	}
+
+}
+
+func (h *Heap) ExtractMIn() int {
+
+	if len(h.data) == 0 {
+		return -1
+	}
+
+	min := h.data[0]
+
+	last := h.data[len(h.data)-1]
+	h.data[0] = last
+	h.data = h.data[:len(h.data)-1]
+
+	return min
+}
+
+func (h *Heap) HeapifyDown(index int) {
+
+	size := len(h.data)
+
+	for {
+
+		left := 2*index + 1
+		right := 2*index + 2
+		smallest := index
+
+		if left < size && h.data[left] < h.data[smallest] {
+			smallest = left
+		}
+
+		if right < size && h.data[right] < h.data[smallest] {
+			smallest = right
+		}
+
+		if index == smallest {
+			break
+		}
+
+		h.data[index], h.data[smallest] = h.data[smallest], h.data[index]
+		index = smallest
+
+	}
+
+}
+
+func main() {
+
+	h := Heap{}
+	x := Heap{}
+	h.MinInsert(10)
+	h.MinInsert(5)
+	h.MinInsert(30)
+	h.MinInsert(2)
+	h.MinInsert(1)
+	x.MaxInsert(10)
+	x.MaxInsert(20)
+	x.MaxInsert(50)
+
+	fmt.Println(h.data)
+	fmt.Println(x.data)
+}
+
+
 
 func (h *Heap) MinInsert(value int) {
 
@@ -144,35 +126,4 @@ func (h *Heap) ExtractMax() int {
 	return max
 
 }
-
-func (h *Heap) HeapifyDown(index int) {
-
-	size := len(h.data)
-
-	for {
-
-		left := 2*index + 1
-		right := 2*index + 2
-		largest := index
-
-		if left < size && h.data[left] > h.data[largest] {
-			largest = left
-		}
-
-		if right < size && h.data[right] > h.data[largest] {
-			largest = right
-		}
-
-		if largest == index {
-			break
-		}
-
-		h.data[index], h.data[largest] = h.data[largest], h.data[index]
-
-		index = largest
-
-	}
-
-}
-
 
--- sorting/test/main.go
+++ sorting/test/main.go
@@ -5,8 +5,8 @@ func BubbleSort(arr []int) {
 	n := len(arr)
 
 	for i := 0; i < n-1; i++ {
-
 		swapped := false
+
 		for j := 0; j < n-i-1; j++ {
 			if arr[j] > arr[j+1] {
 				arr[j], arr[j+1] = arr[j+1], arr[j]
@@ -22,10 +22,30 @@ func BubbleSort(arr []int) {
 
 }
 
+func SelectionSort(arr []int) {
+
+	size := len(arr)
+
+	for i := 0; i < size-1; i++ {
+		minIndex := i
+
+		for j := i + 1; j < size; j++ {
+			if arr[minIndex] > arr[j] {
+				minIndex = j
+			}
+		}
+
+		arr[i], arr[minIndex] = arr[minIndex], arr[i]
+	}
+
+}
+
 func InsertionSort(arr []int) {
+
 	n := len(arr)
 
-	for i := 0; i < n-1; i++ {
+	for i := 1; i < n; i++ {
+
 		key := arr[i]
 		j := i - 1
 
@@ -35,42 +55,23 @@ func InsertionSort(arr []int) {
 		}
 
 		arr[j+1] = key
-
-	}
-
-}
-
-func SelectionSort(arr []int) {
-
-	n := len(arr)
-
-	for i := 0; i < n-1; i++ {
-		minIndex := i
-
-		for j := 1; j < n; j++ {
-			if arr[minIndex] > arr[j] {
-				minIndex = j
-			}
-		}
-
-		arr[i], arr[minIndex] = arr[minIndex], arr[i]
-
 	}
 
 }
 
 type Node struct {
 	data  int
-	right *Node
 	left  *Node
+	right *Node
 }
 
 func Insert(root *Node, value int) *Node {
+
 	if root == nil {
 		return &Node{data: value}
 	}
 
-	if value < root.data {
+	if root.data > value {
 		root.left = Insert(root.left, value)
 	} else {
 		root.right = Insert(root.right, value)
@@ -90,11 +91,11 @@ func Search(root *Node, value int) bool {
 		return true
 	}
 
-	if value < root.data {
-		return Search(root.left, value)
+	if value > root.data {
+		return Search(root.right, value)
 	}
 
-	return Search(root.right, value)
+	return Search(root.left, value)
 
 }
 
@@ -109,35 +110,39 @@ func Delete(root *Node, value int) *Node {
 	} else if value > root.data {
 		root.right = Delete(root.right, value)
 	} else {
+
 		if root.left == nil {
 			return root.right
 		}
-
 		if root.right == nil {
 			return root.left
 		}
 
-		minNode := findMin(root.right)
+		minNode := minNode(root.right)
 		root.data = minNode.data
 		root.right = Delete(root.right, minNode.data)
 
 	}
+
 	return root
 
 }
 
-func findMin(root *Node) *Node {
+func minNode(root *Node) *Node {
+
 	for root.left != nil {
 		root = root.left
 	}
+
 	return root
+
 }
 
 type Heap struct {
 	arr []int
 }
 
-func (h *Heap) InsetMin(value int) {
+func (h *Heap) HeapifyUp(value int) {
 
 	h.arr = append(h.arr, value)
 	index := len(h.arr) - 1
@@ -146,8 +151,8 @@ func (h *Heap) InsetMin(value int) {
 
 		parent := (index - 1) / 2
 
-		if h.arr[parent] > h.arr[index] {
-			h.arr[parent], h.arr[index] = h.arr[index], h.arr[parent]
+		if h.arr[index] > h.arr[parent] {
+			h.arr[index], h.arr[parent] = h.arr[parent], h.arr[index]
 			index = parent
 		} else {
 			break
@@ -156,20 +161,20 @@ func (h *Heap) InsetMin(value int) {
 	}
 
 }
-func (h *Heap) ExtractMIn() int {
+
+func (h Heap) ExtractMax() int {
 
 	if len(h.arr) == 0 {
 		return -1
 	}
 
-	min := h.arr[0]
 	n := len(h.arr) - 1
+	maxVale := h.arr[0]
 	h.arr[0] = h.arr[n]
 	h.arr = h.arr[:n]
 
 	h.HeapifyDown(0)
-
-	return min
+	return maxVale
 
 }
 
@@ -181,64 +186,43 @@ func (h *Heap) HeapifyDown(index int) {
 
 		left := 2*index + 1
 		right := 2*index + 2
-		smallest := index
+		largest := index
 
-		if left < size && h.arr[left] < h.arr[smallest] {
-			smallest = left
+		if left < size && h.arr[left] > h.arr[largest] {
+			largest = left
 		}
 
-		if right < size && h.arr[right] < h.arr[smallest] {
-			smallest = right
+		if right < size && h.arr[right] > h.arr[largest] {
+			largest = right
 		}
 
-		if index == smallest {
+		if largest == index {
 			break
 		}
 
-		h.arr[index], h.arr[smallest] = h.arr[smallest], h.arr[index]
-		index = smallest
+		h.arr[index], h.arr[largest] = h.arr[largest], h.arr[index]
+		index = largest
 
 	}
 
 }
 
-func BubbleSortu(arr []int) {
+func BinarySearch(arr []int, target int) int {
 
-	n := len(arr)
+	low := 0
+	high := len(arr) - 1
 
-	for i := 0; i < n-1; i++ {
-		swapped := false
+	for low <= high {
+		mid := low + (high-low)/2
 
-		for j := 0; j < n-i-1; j++ {
-			if arr[j+1] < arr[j] {
-				arr[j], arr[j+1] = arr[j+1], arr[j]
-				swapped = true
-			}
-		}
-
-		if !swapped {
-			break
+		if arr[mid] == target {
+			return mid
+		} else if arr[mid] < target {
+			low = mid + 1
+		} else {
+			high = mid - 1
 		}
 
 	}
-
-}
-
-func InsertionSorts(arr []int) {
-
-	n := len(arr)
-
-	for i := 1; i < n; i++ {
-		key := arr[i]
-		j := i - 1
-
-		for j >= 0 && arr[j] > key {
-			arr[j+1] = arr[j]
-			j--
-		}
-
-		arr[j+1] = key
-
-	}
-
+	return -1
 }
