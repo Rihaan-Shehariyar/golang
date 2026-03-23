0ff12fe95c309a1bc46cb005c34f5cf6fafb5761
Rihaan Shehariyar <rihaanshehariyar@gmail.com>
2026-03-23 16:47:24 +0530

    updated

--- Graph/main.go
+++ Graph/main.go
@@ -2,25 +2,22 @@ package main
 
 import "fmt"
 
-// Graph structure
 type Graph struct {
 	adjList map[int][]int
 }
 
-// Initialize graph
+
 func NewGraph() *Graph {
 	return &Graph{
 		adjList: make(map[int][]int),
 	}
 }
 
-// Add edge (undirected)
 func (g *Graph) AddEdge(u, v int) {
 	g.adjList[u] = append(g.adjList[u], v)
 	g.adjList[v] = append(g.adjList[v], u)
 }
 
-// Print graph
 func (g *Graph) Print() {
 	for node, neighbors := range g.adjList {
 		fmt.Printf("%d -> %v\n", node, neighbors)
