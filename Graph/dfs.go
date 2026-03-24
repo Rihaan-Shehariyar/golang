6824d01fc98446b62a854674a62ba3b6f5638378
Rihaan Shehariyar <rihaanshehariyar@gmail.com>
2026-03-24 19:45:19 +0530

    updated

---
+++ Graph/dfs.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+type GraphDfs struct {
+	adj map[int][]int
+}
+
+func (g *GraphDfs) AddEdge(u, v int) {
+	g.adj[u] = append(g.adj[u], v)
+	g.adj[v] = append(g.adj[v], u)
+}
+
+func (g *GraphDfs) DfSStack(start int) {
+	visited := make(map[int]bool)
+
+	stack := []int{start}
+
+	for len(stack) > 0 {
+		node := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		if !visited[node] {
+			visited[node] = true
+			fmt.Print(node, "")
+
+			for _, neigbour := range g.adj[node] {
+				if !visited[neigbour] {
+					stack = append(stack, neigbour)
+				}
+			}
+
+		}
+
+	}
+
+}
+
+func main() {
+
+	g := GraphDfs{
+		adj: make(map[int][]int),
+	}
+
+	g.AddEdge(1, 2)
+	g.AddEdge(1, 3)
+	g.AddEdge(2, 4)
+
+	g.DfSStack(1)
+
+}
--- Graph/main.go
+++ Graph/main.go
@@ -15,8 +15,8 @@ func (g *Graph) AddEdge(u, v int) {
 	g.adjList[v] = append(g.adjList[v], u)
 }
 
-func (g *Graph) AddDirectEdge(u, v int) {
-	g.adjList[u] = append(g.adjList[u], v)
+func (g *Graph) AddDirectEdge(u,v int){
+  g.adjList[u] = append(g.adjList[u], v)
 }
 
 func (g *Graph) PrintGraph() {
@@ -25,17 +25,18 @@ func (g *Graph) PrintGraph() {
 	}
 }
 
-func main() {
-	g := NewGraph()
+// func main() {
+// 	g := NewGraph()
 
-	// g.AddEdge(1, 2)
-	// g.AddEdge(1, 3)
-	// g.AddEdge(2, 4)
+// 	// g.AddEdge(1, 2)
+// 	// g.AddEdge(1, 3)
+// 	// g.AddEdge(2, 4)
 
-	g.AddDirectEdge(1, 2)
-	g.AddDirectEdge(1, 3)
-	g.AddDirectEdge(2, 4)
+//  g.AddDirectEdge(1,2)
+//  g.AddDirectEdge(1,3)
+//  g.AddDirectEdge(1,3)
+//  g.AddDirectEdge(1,3)
 
-	g.PrintGraph()
+//  g.PrintGraph()
 
-}
+// }
