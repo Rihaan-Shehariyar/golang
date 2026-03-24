5e1a3d8d9b5cb36d540ff22a5917b4a21ca6ca31
Rihaan Shehariyar <rihaanshehariyar@gmail.com>
2026-03-24 10:20:52 +0530

    updated

--- Graph/main.go
+++ Graph/main.go
@@ -15,6 +15,10 @@ func (g *Graph) AddEdge(u, v int) {
 	g.adjList[v] = append(g.adjList[v], u)
 }
 
+func (g *Graph) AddDirectEdge(u, v int) {
+	g.adjList[u] = append(g.adjList[u], v)
+}
+
 func (g *Graph) PrintGraph() {
 	for node, neighbours := range g.adjList {
 		fmt.Printf("%d->%v\n", node, neighbours)
@@ -24,10 +28,14 @@ func (g *Graph) PrintGraph() {
 func main() {
 	g := NewGraph()
 
-	g.AddEdge(1, 2)
-	g.AddEdge(1, 3)
-	g.AddEdge(2, 4)
+	// g.AddEdge(1, 2)
+	// g.AddEdge(1, 3)
+	// g.AddEdge(2, 4)
 
- g.PrintGraph()
+	g.AddDirectEdge(1, 2)
+	g.AddDirectEdge(1, 3)
+	g.AddDirectEdge(2, 4)
+
+	g.PrintGraph()
 
 }
