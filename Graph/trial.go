5e01b598e15746c5f17dcb5f61171dbf068c199d
Rihaan Shehariyar <rihaanshehariyar@gmail.com>
2026-03-27 20:50:06 +0530

    updated

--- Graph/trial.go
+++ Graph/trial.go
@@ -64,3 +64,127 @@ func (g *graphs) dfsrec(node int, visited map[int]bool) {
 
 }
 
+func (g *graphs) bfs(start int) {
+
+	visited := make(map[int]bool)
+	visited[start] = true
+	queue := []int{start}
+
+	for len(queue) > 0 {
+		node := queue[0]
+		queue = queue[1:]
+
+		fmt.Print(node, "")
+
+		for _, neigbour := range g.adjList[node] {
+			if !visited[neigbour] {
+				queue = append(queue, neigbour)
+				visited[neigbour] = true
+			}
+		}
+
+	}
+
+}
+
+func QuickSorts(arr []int, low, high int) {
+
+	for low < high {
+		pivotIndex := part(arr, low, high)
+
+		QuickSorts(arr, low, pivotIndex-1)
+		QuickSorts(arr, pivotIndex+1, high)
+
+	}
+
+}
+
+func part(arr []int, low, high int) int {
+
+	pivotIndex := arr[high]
+	i := low - 1
+
+	for j := low; j < high; j++ {
+		if arr[j] < pivotIndex {
+			i++
+			arr[j], arr[i] = arr[i], arr[j]
+		}
+	}
+
+	arr[i+1], arr[high] = arr[high], arr[i+1]
+	return i + 1
+
+}
+
+func mergeSorts(arr []int) []int {
+
+	if len(arr) <= 1 {
+		return arr
+	}
+
+	mid := len(arr) / 2
+
+	left := mergeSorts(arr[:mid])
+	right := mergeSorts(arr[mid:])
+
+	return merges(left, right)
+
+}
+
+func merges(left, right []int) []int {
+
+	result := []int{}
+
+	i, j := 0, 0
+
+	for i < len(left) && j < len(right) {
+		if left[i] > right[j] {
+			result = append(result, left[i:]...)
+			i++
+		} else {
+			result = append(result, right[j:]...)
+			j++
+		}
+	}
+
+	result = append(result, left[i:]...)
+	result = append(result, right[j:]...)
+
+	return result
+
+}
+
+func HeapSorts(arr []int) {
+
+	n := len(arr)
+
+	for i := n/2 - 1; i >= 0; i-- {
+		heapifys(arr, n, i)
+	}
+
+	for i := n - 1; i > 0; i-- {
+		arr[0], arr[i] = arr[i], arr[0]
+
+		heapifys(arr, i, 0)
+	}
+
+}
+
+func heapifys(arr []int, n, i int) {
+	largest := i
+	left := 2*n + 1
+	right := 2*n + 2
+
+	if n < left && arr[left] > arr[largest] {
+		largest = left
+	}
+	if n < right && arr[right] > arr[largest] {
+		largest = right
+	}
+
+	if i != largest {
+		arr[i], arr[largest] = arr[largest], arr[i]
+		heapifys(arr, n, largest)
+	}
+
+}
