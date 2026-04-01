e0a42ee5d4a9f18f76d0106c4981ef4a15aa1797
Rihaan Shehariyar <rihaanshehariyar@gmail.com>
2026-04-01 15:27:17 +0530

    updated

--- gRPC/client/main.go
+++ gRPC/client/main.go
@@ -2,32 +2,59 @@ package main
 
 import (
 	"context"
+	pb "grpc/user/proto"
 	"log"
 	"time"
-
-	pb "grpc/user/proto"
 
 	"google.golang.org/grpc"
 )
 
+// func main() {
+// 	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer conn.Close()
+
+// 	client := pb.NewHelloServiceClient(conn)
+
+// 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+// 	defer cancel()
+
+// 	res, err := client.SayHello(ctx, &pb.HelloRequest{
+// 		Name: "Rihaan",
+// 	})
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	log.Println("Response:", res.Message)
+// }
+
 func main() {
+
 	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	defer conn.Close()
 
 	client := pb.NewHelloServiceClient(conn)
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+
 	defer cancel()
 
 	res, err := client.SayHello(ctx, &pb.HelloRequest{
-		Name: "Rihaan",
+		Name: "Abu sala",
 	})
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Println("Response:", res.Message)
+	log.Println("Response : ", res.Message)
+
 }
--- gRPC/server/main.go
+++ gRPC/server/main.go
@@ -2,45 +2,75 @@ package main
 
 import (
 	"context"
+	pb "grpc/user/proto"
 	"log"
 	"net"
-
-	pb "grpc/user/proto"
 
 	"google.golang.org/grpc"
 )
 
-// server struct implements the gRPC service
+// // server struct implements the gRPC service
+// type server struct {
+// 	pb.UnimplementedHelloServiceServer
+// }
+
+// // SayHello is the RPC method implementation
+// func (s *server) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloResponse, error) {
+// 	log.Println("Received:", req.Name)
+
+// 	return &pb.HelloResponse{
+// 		Message: "Hello " + req.Name,
+// 	}, nil
+// }
+
+// func main() {
+// 	// 1. Create TCP listener
+// 	lis, err := net.Listen("tcp", ":50051")
+// 	if err != nil {
+// 		log.Fatal("Failed to listen:", err)
+// 	}
+
+// 	// 2. Create gRPC server
+// 	grpcServer := grpc.NewServer()
+
+// 	// 3. Register service
+// 	pb.RegisterHelloServiceServer(grpcServer, &server{})
+
+// 	log.Println("🚀 Server running on port 50051...")
+
+// 	// 4. Start server
+// 	if err := grpcServer.Serve(lis); err != nil {
+// 		log.Fatal("Failed to serve:", err)
+// 	}
+// }
+
 type server struct {
 	pb.UnimplementedHelloServiceServer
 }
 
-// SayHello is the RPC method implementation
 func (s *server) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloResponse, error) {
-	log.Println("Received:", req.Name)
+	log.Println("Recieved : ", req.Name)
 
 	return &pb.HelloResponse{
-		Message: "Hello " + req.Name,
+		Message: "Hello" + req.Name,
 	}, nil
+
 }
 
 func main() {
-	// 1. Create TCP listener
 	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
-		log.Fatal("Failed to listen:", err)
+		log.Fatal("Failed To Listen", err)
 	}
 
-	// 2. Create gRPC server
 	grpcServer := grpc.NewServer()
 
-	// 3. Register service
 	pb.RegisterHelloServiceServer(grpcServer, &server{})
 
-	log.Println("🚀 Server running on port 50051...")
+	log.Println("Server Running on Port 50051")
 
-	// 4. Start server
 	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatal("Failed to serve:", err)
+		log.Fatalln("Failed To serve : ", err)
 	}
+
 }
