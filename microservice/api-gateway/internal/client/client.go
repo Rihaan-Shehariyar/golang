ee74b5e5090f45d0e96136df46e1c784693c2b81
Rihaan Shehariyar <rihaanshehariyar@gmail.com>
2026-04-03 11:33:14 +0530

    updated

---
+++ microservice/api-gateway/internal/client/client.go
@@ -0,0 +1,31 @@
+package client
+
+import (
+	"log"
+	orderpb "shared-proto/order"
+	userpb "shared-proto/user"
+
+	"google.golang.org/grpc"
+)
+
+type Clients struct {
+	UserClient  userpb.UserServiceClient
+	OrderClient orderpb.OrderServiceClient
+}
+
+func NewClients() *Clients {
+	userConn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+	orderConn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &Clients{
+		UserClient:  userpb.NewUserServiceClient(userConn),
+		OrderClient: orderpb.NewOrderServiceClient(orderConn),
+	}
+
+}
